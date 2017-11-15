91f0f1379f89999e26ed3833cf7b6ab0507f49c9
Jean Prunneaux <jean@prunneaux.com>
2017-11-15 17:36:04 +0100

    Minor improvements; do not copy locales README

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# libbash debian package
+# libbash.sh debian package
+
+This project is meant to build the libbash.sh debian package.
 
 ## Build instructions
-1. Clone libbash in `libbash` directory.
+1. Clone libbash.sh here in `libbash.sh` directory
 2. Run `./build.sh` script
+3. Package and checksum is in the `build/` directory
+
+## License
+libbash.sh is licensed under the MIT License. See [LICENSE.md](LICENSE.md) for the full license text.
+
+## Credits
+Author: Jean Prunneaux  [http://jean.prunneaux.com](http://jean.prunneaux.com)
+
+Website: [https://github.com/pruje/libbash.sh](https://github.com/pruje/libbash.sh)
+
+Help us to improve libbash.sh in [submitting issues](https://github.com/pruje/libbash.sh/issues) to report a bug or request new features!
--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#  Build script for time2backup debian package
+#  Build script to build libbash.sh debian package
 #
 #  Project: https://github.com/pruje/libbash.sh
 #  MIT License
@@ -13,23 +13,25 @@
 current_directory=$(dirname "$0")
 
 # test if libbash is there
-if ! [ -d "$current_directory/libbash" ] ; then
-	echo "ERROR: you must put libbash sources in the libbash directory!"
+if ! [ -d "$current_directory/libbash.sh" ] ; then
+	echo "ERROR: you must put sources in the libbash.sh directory!"
 	exit 1
 fi
 
 # get time2backup version
-version=$(grep "^lb_version=" "$current_directory/libbash/libbash.sh" | head -1 | cut -d= -f2)
+version=$(grep "^lb_version=" "$current_directory/libbash.sh/libbash.sh" | head -1 | cut -d= -f2)
 if [ -z "$version" ] ; then
 	echo "ERROR: Cannot get libbash version!"
 	exit 1
 fi
 
-echo -n "Build debian package for libbash $version? (y/N)"
+echo -n "Build debian package for libbash v$version? (y/N) "
 read confirm
 if [ "$confirm" != "y" ] ; then
 	exit
 fi
+
+echo
 
 # create build environment
 mkdir -p "$current_directory/build"
@@ -41,8 +43,8 @@ fi
 package="$current_directory/build/package"
 install_path="$package/usr/lib/libbash"
 
-# clear and copy package files
-echo "Copy package..."
+# clean and copy package files
+echo "Clean and copy package..."
 rm -rf "$package" && cp -rp "$current_directory/package" "$current_directory/build/"
 if [ $? != 0 ] ; then
 	echo "ERROR while copying package files. Please verify your access rights."
@@ -58,15 +60,25 @@ fi
 
 echo "Copy libbash sources..."
 
-mkdir -p "$install_path"
+# create directories
+mkdir -p "$install_path/locales"
 if [ $? != 0 ] ; then
 	echo "ERROR while copying sources files. Please verify your access rights."
 	exit 1
 fi
 
-# copy only useful files
-for f in libbash.sh libbash_gui.sh locales ; do
-	cp -r "$current_directory/libbash/$f" "$install_path"
+# copy libbash main scripts
+for f in libbash.sh libbash_gui.sh ; do
+	cp "$current_directory/libbash.sh/$f" "$install_path"
+	if [ $? != 0 ] ; then
+		echo "ERROR while copying sources files. Please verify your access rights."
+		exit 1
+	fi
+done
+
+# copy locales
+for f in "$current_directory/libbash.sh/locales"/*.sh ; do
+	cp "$f" "$install_path/locales/"
 	if [ $? != 0 ] ; then
 		echo "ERROR while copying sources files. Please verify your access rights."
 		exit 1
@@ -74,7 +86,7 @@ for f in libbash.sh libbash_gui.sh locales ; do
 done
 
 echo "Set permissions..."
-sudo chown -R root:root "$install_path" && sudo chmod -R 755 "$install_path"
+chmod -R 755 "$install_path" && sudo chown -R root:root "$install_path"
 
 # go into the build directory
 cd "$current_directory/build"
@@ -86,7 +98,6 @@ fi
 # set archive name
 archive="libbash.sh_$version.deb"
 
-echo
 echo "Generating deb package..."
 
 dpkg-deb --build package $archive
@@ -116,17 +127,15 @@ if [ $? != 0 ] ; then
 	exit 4
 fi
 
-echo
-echo "Generating checksums..."
+echo "Generating checksum..."
 sha256sum $archive > sha256sum.txt
 if [ $? != 0 ] ; then
 	echo "...Failed!"
 	exit 6
 fi
 
-echo
 echo "Clean files..."
 sudo rm -rf ../package
 
 echo
-echo "Ready to deploy!"
+echo "Package is ready!"
